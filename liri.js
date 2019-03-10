ac1c24bca179e132c227ce3ee1a24fb83f716ed5
travishill1 <travishill1@gmail.com>
2019-03-09 21:04:17 -0600

    spotify-this-song fully functional, do-what-it-says almost functional

--- liri.js
+++ liri.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+var fs = require("fs");
 var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
 var moment = require('moment');
@@ -10,23 +11,21 @@ let command = process.argv[2];
 let inputArg = process.argv.slice(3).join("+");
 
 
-// Make it so liri.js can take in one of the following commands:
+// liri.js can take in one of the following commands:
 // concert-this
 // spotify-this-song
 // movie-this
 // do-what-it-says
 
-
-
 // 1. node liri.js concert-this <artist/band name here>
-    // This will search the Bands in Town Artist Events API ("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp") for an artist and render the following information about each event to the terminal:
-
-    // Name of the venue
-    // Venue location
-    // Date of the Event (use moment to format this as "MM/DD/YYYY")
+    // This will search the Bands in Town Artist Events API for an artist and render the following information about each event to the terminal:
+        // Name of the venue
+        // Venue location
+        // Date of the Event (use moment to format this as "MM/DD/YYYY")
 
     if (command === "concert-this"){
         getConcert();
+
     function getConcert (input) {
             if (!input) {
                 var artist = "daughters";
@@ -38,59 +37,64 @@ let inputArg = process.argv.slice(3).join("+");
             console.log(queryUrl);
         axios.get(queryUrl).then(
             function(response) {
+                console.log("-----------------------------------------------------");
                 console.log("Name of Venue: " + response.data[0].venue.name);
                 console.log("Location: " + response.data[0].venue.city);
                 console.log("Date: " + (moment(response.data[0].datetime)).format("MM/DD/YYYY"));
+                console.log("-----------------------------------------------------");
             }
           );
     
-    };
     }
-
-
-    
+    };
 
 // 2. node liri.js spotify-this-song '<song name here>'
     // This will show the following information about the song in your terminal/bash window
 
-    // Artist(s)
-    // The song's name
-    // A preview link of the song from Spotify
-    // The album that the song is from
+        // Artist(s)
+        // The song's name
+        // A preview link of the song from Spotify
+        // The album that the song is from
 
-    // *If no song is provided then your program will default to "The Sign" by Ace of Base.
-// You will utilize the node-spotify-api package in order to retrieve song information from the Spotify API.
+    // If no song is provided then your program will default to "The Sign" by Ace of Base.
 
-// The Spotify API requires you sign up as a developer to generate the necessary credentials. You can follow these steps in order to generate a client id and client secret:
+if (command === "spotify-this-song"){
+    getSpotify();
 
-// Step One: Visit https://developer.spotify.com/my-applications/#!/
+function getSpotify(input) {
+    if (!inputArg) {
+        var song = "the+sign+ace+of+base";
+    } else {
+        var song = inputArg;
+    }
 
-// Step Two: Either login to your existing Spotify account or create a new one (a free account is fine) and log in.
-
-// Step Three: Once logged in, navigate to https://developer.spotify.com/my-applications/#!/applications/create to register a new application to be used with the Spotify API. 
-// You can fill in whatever you'd like for these fields. When finished, click the "complete" button.
-
-// Step Four: On the next screen, scroll down to where you see your client id and client secret. Copy these values down somewhere, you'll need them to use the Spotify API and the node-spotify-api package.
-
-
-
-
+    spotify.search({type:"track", query: song}, function(err,response){
+      if (err){
+        console.log(err)
+      }
+      console.log("-----------------------------------------------------");
+      console.log("Artist: " + response.tracks.items[0].album.artists[0].name);
+      console.log("Song Name: " + response.tracks.items[0].name);
+      console.log("Album: " + response.tracks.items[0].album.name);
+      console.log("Preview: " + response.tracks.items[0].preview_url);
+      console.log("-----------------------------------------------------");
+    })
+}
+};
 
 
 // 3. node liri.js movie-this '<movie name here>'
-
 // This will output the following information to your terminal/bash window:
-
-//   * Title of the movie.
-//   * Year the movie came out.
-//   * IMDB Rating of the movie.
-//   * Rotten Tomatoes Rating of the movie.
-//   * Country where the movie was produced.
-//   * Language of the movie.
-//   * Plot of the movie.
-//   * Actors in the movie.
+    //   Title of the movie.
+    //   Year the movie came out.
+    //   IMDB Rating of the movie.
+    //   Rotten Tomatoes Rating of the movie.
+    //   Country where the movie was produced.
+    //   Language of the movie.
+    //   Plot of the movie.
+    //   Actors in the movie.
 // If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
-// You'll use the axios package to retrieve data from the OMDB API. Like all of the in-class activities, the OMDB API requires an API key. You may use trilogy.
+
 if (command === "movie-this"){
     getMovie();
 function getMovie (input) {
@@ -104,6 +108,7 @@ function getMovie (input) {
         console.log(queryUrl);
     axios.get(queryUrl).then(
         function(response) {
+          console.log("-----------------------------------------------------");
           console.log('*Title of the Movie: ' + response.data.Title)
           console.log("*Release Year: " + response.data.Year);
           console.log("*IMDB Rating: " + response.data.Ratings[0].Value);
@@ -112,16 +117,12 @@ function getMovie (input) {
           console.log("*Language: " + response.data.Language);
           console.log("*Plot: " + response.data.Plot);
           console.log("*Actors: " + response.data.Actors);
+          console.log("-----------------------------------------------------");
         }
       );
 
-};
 }
-
-
-
-
-
+};
 
 // 4. node liri.js do-what-it-says
 
@@ -129,10 +130,19 @@ function getMovie (input) {
 // It should run spotify-this-song for "I Want it That Way," as follows the text in random.txt.
 // Edit the text in random.txt to test out the feature for movie-this and concert-this.
 
+if (command === "do-what-it-says"){
+    getWhatItSays();
 
-
-
-// BONUS
-// In addition to logging the data to your terminal/bash window, output the data to a .txt file called log.txt.
-// Make sure you append each command you run to the log.txt file.
-// Do not overwrite your file each time you run a command.
+function getWhatItSays (input) {
+    fs.readFile("random.txt","utf8",function(err,data){
+        if (err){
+        console.log(err)
+        }
+        let readCommand = data.split(",");
+        command = readCommand[0]
+        inputArg = readCommand[1]
+        console.log(command);
+        console.log(inputArg);
+    })
+}
+}
